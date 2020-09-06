35bbd51cb8142da9005f27ee9ea4cd7d33b9db2a
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2020-09-06 13:39:40 -0600

    initial commit for project

--- index.js
+++ index.js
@@ -2,44 +2,44 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *    Counter 1 has let count=0 within the function. Counter 2 has let count=0 outside the function.
+ * 2. Which of the two uses a closure? How can you tell?
+ *    Counter 2 code. Because it has to reach outside the function to find the data and access let count=0.
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *Counter 1 woud be preferable in
+ *Counter 2 would be preferable in
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,20 +51,33 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+const inningScore = (max) => {
+  return Math.floor(Math.random() * max);
+};
+function inning(cb) {
+  let home = 0;
+  let away = 0;
+  return {
+    home: (home += cb(3)),
+    away: (away += cb(3)),
+  };
 }
+console.log("Task 2", inning(inningScore));
+
+// scoreboard(getInningScore, inning, numInnnings) {
+//   // code
+//   getInningScore(inning);
+// }
 
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+
+Definition-A higher-order function is a function that can take another function as an argument, or that returns a function as a result.
 
 For example, 
 
@@ -74,13 +87,19 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore() {
+   let home= 0, 
+   let away= 0, 
+   let score={Home:home, Away:away}
+   return function (){
+     score.Home= socre.Home.inning()
+     score.Away= score.Away +inning()
+       return score 
+   }
 }
+console.log(finalScore(inning, 7))
 
 /* Task 4: 
 
@@ -102,9 +121,6 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
-
-
